c25e3fb880ee79e0c3cb902d9da8e641b857770d
rosshkaa <rosshka@gmail.com>
2017-04-13 03:20:44 +0300

    Подключены qss.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include "widget.h"
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
 
-    QPalette pal;
-   pal.setBrush((&w)->backgroundRole(), QBrush(QPixmap("D:\\backgr.jpg")));
-
-    (&w)->setPalette(pal);
-    (&w)->setAutoFillBackground(true);
+    QFile file(":/style/style.qss");
+    if(file.open(QFile::ReadOnly)) {
+       QString StyleSheet = QLatin1String(file.readAll());
+       qApp->setStyleSheet(StyleSheet);
+    }
+    else {
+        qDebug() << "Stylesheet load failed";
+    };
 
     w.show();
 
